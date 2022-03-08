33241035ec3a33a5187b080cdd82b57f8a417d22
Eunah323 <unapretty@gmail.com>
2022-03-08 17:00:41 +0900

    중간점검 전 커밋입니다. 실시간 하트수 가져오는 get은 아직 구현하지 못했습니다.

--- app.py
+++ app.py
@@ -2,3 +2,18 @@ from pymongo import MongoClient
 
 client = MongoClient('mongodb+srv://test:sparta@cluster0.e5mxe.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
+
+@app.route("/posts/like", methods=["POST"])
+def like_post():
+    like_receive = request.form['like_give']
+    bucket_list = list(db.bucket.find({}, {'_id': False}))
+    num = len(bucket_list) + 1
+
+    doc = {
+        'candidate_num': num,
+        'id': jwt방식에서 가져온 ID ,
+        'done': 0
+    }
+
+
+    db.bucket.insert_one(doc)
