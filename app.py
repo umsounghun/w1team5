5392d597bdebefb925770affa026aeb774267a48
cge1023 <ge931023@gmail.com>
2022-03-08 03:08:00 -0800

    DB수정, 애매한 코드에 주석 추가

--- app.py
+++ app.py
@@ -1,31 +1,27 @@
-from flask import Flask, render_template, jsonify, request, session, redirect, url_for
-
-app = Flask(__name__)
-
 from pymongo import MongoClient
+import jwt
+import hashlib
+from flask import Flask, render_template, jsonify, request, redirect, url_for
 import certifi
 
 ca = certifi.where()
 
-from pymongo import MongoClient
-import jwt
-import hashlib
+client = MongoClient('mongodb+srv://test:sparta@cluster0.ezfes.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
+db = client.dbsparta
 
+app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
 
 SECRET_KEY = 'SPARTA'
-
-client = MongoClient('mongodb://3.35.208.24', 27017, username="test", password="test")
-db = client.dbsparta_plus_week4
 
 @app.route('/')
 def home():
     token_receive = request.cookies.get('mytoken')
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-
-        return render_template('index.html')
+        user_info = db.users.find_one({"username": payload["id"]})
+        return render_template('index.html', user_info=user_info)
     except jwt.ExpiredSignatureError:
         return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
     except jwt.exceptions.DecodeError:
@@ -72,9 +68,6 @@ def sign_in():
     else:
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
-@app.route('/login', methods=['GET'])
-def
-
 
 # @app.route('/sign_up/save', methods=['POST'])
 # def sign_up():
@@ -90,8 +83,8 @@ def
 # def check_dup():
 #     # ID 중복확인
 #     return jsonify({'result': 'success'})
-#
-#
+
+
 # @app.route('/update_profile', methods=['POST'])
 # def save_img():
 #     token_receive = request.cookies.get('mytoken')
@@ -103,27 +96,35 @@ def
 #         return redirect(url_for("home"))
 
 
-@app.route('/posting', methods=['POST'])
+@app.route('/posting', methods=['POST']) #댓글 작성 하기
 def posting():
-    token_receive = request.cookies.get('mytoken')
+    token_receive = request.cookies.get('mytoken') #token_receive로 쿠키를 가져옴
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        # 포스팅하기
+        user_info = db.users.find_one({"username": payload["id"]}) #payload값에 id값이 있기때문 유저정보를 항상 알 수 있음
+        comment_receive = request.form["comment_give"]
+        # cannum_receive = request.form["cannum_give"]
+        doc = {
+            "username": user_info["username"],
+            "comment": comment_receive
+        }
+        db.posts.insert_one(doc)
         return jsonify({"result": "success", 'msg': '포스팅 성공'})
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
 
 
-@app.route("/get_posts", methods=['GET'])
+@app.route("/get_posts", methods=['GET']) #작성된 댓글 가져오기
 def get_posts():
     token_receive = request.cookies.get('mytoken')
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        # 포스팅 목록 받아오기
-        return jsonify({"result": "success", "msg": "포스팅을 가져왔습니다."})
+        posts = list(db.posts.find({}).sort("date",-1).limit(20)) #작성된 날짜순(최신순)으로 최대 20개 댓글 가져오기
+        for post in posts:
+            post["_id"] = str(post["_id"]) #각각의 id를 문자열로 바꾼다
+        return jsonify({"result": "success", "msg": "포스팅을 가져왔습니다.","posts":posts})
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
-
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
