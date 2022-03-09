f77d6d302cd212757d2ed07e7aabf311d1f079a1
seonsik <suns2131@naver.com>
2022-03-09 10:37:51 +0900

    추가-app.py 업데이트 #1
    스케줄러 패키지를 통하여 스케줄 관리

--- app.py
+++ app.py
@@ -49,43 +49,9 @@ schedule.every(30).minutes.do(Crowling)
 
 @app.route('/')
 def home():
-    posting = list(db.candidate.find({}, {'_id': False}))
-    print(posting)
-    return render_template('index.html', list = posting )
-
-@app.route("/candidates", methods=["GET"])
-def can_list_get():
-    #대선 후보 크롤링
-    headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    candidates = requests.get('https://search.naver.com/search.naver?sm=tab_sug.top&where=nexearch&query=20%EB%8C%80+%EB%8C%80%EC%84%A0%ED%9B%84%EB%B3%B4&oquery=20%EB%8C%80+%EB%8C%80%EC%84%A0&tqi=hAkBJdprvhGssLlM2uNssssstJZ-052723&acq=20%EB%8C%80+%EB%8C%80%EC%84%A0&acr=1&qdt=0',headers=headers)
-    soup_candidates = BeautifulSoup(candidates.text, 'html.parser')
-    profile = soup_candidates.select('.list_item')
-    li_image = []
-    li_symbol = []
-    li_name = []
-    li_party = []
-    for can_list in profile:
-        image_list = can_list.select_one('.thumb')
-        if image_list is not None:
-            images = image_list.select('img')
-            for a in images:
-                li_image.append(a['src'])
-    for can_list in profile:
-        symbol_list = can_list.select_one('.thumb')
-        if symbol_list is not None:
-            li_symbol.append(symbol_list.text)
-    for can_list in profile:
-        name_list = can_list.select_one('.name_txt')
-        if name_list is not None:
-            li_name.append(name_list.text)
-    for can_list in profile:
-        party_list = can_list.select_one('.party')
-        if party_list is not None:
-            li_party.append(party_list.text)
-    return jsonify({'symbol' : li_symbol,
-                    'name'  : li_name,
-                    'party'  : li_party,
-                    'image'  : li_image})
+    can_list = list(db.candidate.find({}, {'_id': False}))
+    print(can_list)
+    return render_template('index.html', list = can_list)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
