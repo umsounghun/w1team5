eea98a0e99b6341267e9a6b024a953d094e75655
cge1023 <ge931023@gmail.com>
2022-03-08 19:02:01 -0800

    app.py 로그인 댓글 기능 구현 업데이트/댓글 html css 완성

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import certifi
 
 ca = certifi.where()
 
-client = MongoClient('mongodb+srv://test:sparta@cluster0.ezfes.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
+client = MongoClient('mongodb+srv://test:sparta@cluster0.e5mxe.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
 db = client.dbsparta
 
 app = Flask(__name__)
@@ -33,19 +33,18 @@ def login():
     msg = request.args.get("msg")
     return render_template('login.html', msg=msg)
 
-
-@app.route('/user/<username>')
-def user(username):
-    # 각 사용자의 프로필과 글을 모아볼 수 있는 공간
-    token_receive = request.cookies.get('mytoken')
-    try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        status = (username == payload["id"])  # 내 프로필이면 True, 다른 사람 프로필 페이지면 False
-
-        user_info = db.users.find_one({"username": username}, {"_id": False})
-        return render_template('user.html', user_info=user_info, status=status)
-    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-        return redirect(url_for("home"))
+# @app.route('/user/<username>')
+# def user(username):
+#     # 각 사용자의 프로필과 글을 모아볼 수 있는 공간
+#     token_receive = request.cookies.get('mytoken')
+#     try:
+#         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+#         status = (username == payload["id"])  # 내 프로필이면 True, 다른 사람 프로필 페이지면 False
+#
+#         user_info = db.users.find_one({"username": username}, {"_id": False})
+#         return render_template('user.html', user_info=user_info, status=status)
+#     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+#         return redirect(url_for("home"))
 
 
 @app.route('/sign_in', methods=['POST'])
@@ -95,36 +94,27 @@ def sign_in():
 #     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
 #         return redirect(url_for("home"))
 
+@app.route('/posts')
+def comment():
+    msg = request.args.get("msg")
+    return render_template('posts.html', msg=msg)
 
-@app.route('/posting', methods=['POST']) #댓글 작성 하기
-def posting():
-    token_receive = request.cookies.get('mytoken') #token_receive로 쿠키를 가져옴
-    try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        user_info = db.users.find_one({"username": payload["id"]}) #payload값에 id값이 있기때문 유저정보를 항상 알 수 있음
-        comment_receive = request.form["comment_give"]
-        # cannum_receive = request.form["cannum_give"]
-        doc = {
-            "username": user_info["username"],
-            "comment": comment_receive
-        }
-        db.posts.insert_one(doc)
-        return jsonify({"result": "success", 'msg': '포스팅 성공'})
-    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-        return redirect(url_for("home"))
+@app.route("/posts/comment", methods=["POST"])
+def comment_post():
+    comment_receive = request.form['comment_give']
+    comment_list = list(db.comment.find({}, {'_id': False}))
 
+    doc = {
+        'comment' : comment_receive
+    }
 
-@app.route("/get_posts", methods=['GET']) #작성된 댓글 가져오기
-def get_posts():
-    token_receive = request.cookies.get('mytoken')
-    try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        posts = list(db.posts.find({}).sort("date",-1).limit(20)) #작성된 날짜순(최신순)으로 최대 20개 댓글 가져오기
-        for post in posts:
-            post["_id"] = str(post["_id"]) #각각의 id를 문자열로 바꾼다
-        return jsonify({"result": "success", "msg": "포스팅을 가져왔습니다.","posts":posts})
-    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-        return redirect(url_for("home"))
+    db.comment.insert_one(doc)
+    return jsonify({'msg':'댓글 등록 완료!'})
+
+@app.route("/posts/comment", methods=["GET"])
+def comment_get():
+    comment_list = list(db.comment.find({}, {'_id': False}))
+    return jsonify({'comments': comment_list})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
