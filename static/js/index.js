8136be2812d263ecfe9fcfbb2a478d589957c4f7
seonsik <suns2131@naver.com>
2022-03-09 13:04:20 +0900

    추가-index.html, index.js 업데이트 #2
    index.js에 검색어 정보를 전달받아서 검색된 카드만 보일 수 있도록 하는
    keyword_fillter 함수 구성

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,27 @@
 // index 페이지 내에서 사용할 함수 정의.
 /**
- * 대선 후보
+ * 키워드 검색
+ * 전달받은 검색어 기준으로 해당 되는 카드만 재구성하도록 작업
  */
-function can_list_crowling(symbol, name, party, image)
+function keyword_fillter(keyword)
 {
+    const classnm = document.getElementsByClassName("card_desc");
+    const delarea = document.getElementsByClassName("card_arr");
 
+    for(let i=0; i < classnm.length; i++)
+    {
+        let can_nm = classnm[i].getElementsByClassName('c_name');
+        let can_symbol = classnm[i].getElementsByClassName('c_symbol');
+        let can_party = classnm[i].getElementsByClassName('c_party');
+
+        if(can_nm[0].innerHTML.toLowerCase().indexOf(keyword) != -1)
+        {
+            delarea[i].style.display = "flex";
+        }
+        else
+            delarea[i].style.display = "none";
+        console.log(can_nm,can_symbol,can_party)
+    }
+    console.log(keyword);
 
 }
