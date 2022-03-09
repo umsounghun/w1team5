633f42d424083f3a355c9d07c7d3a42c661ecc6a
Sounghun Um <korea970112@naver.com>
2022-03-09 15:38:10 +0900

    회원가입 주소 추가

--- app.py
+++ app.py
@@ -67,6 +67,10 @@ def home():
 
     return render_template('index.html', list = can_list)
 
+@app.route('/membership')
+def membership():
+    return render_template('membership.html')
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
 
