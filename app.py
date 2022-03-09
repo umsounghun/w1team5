eeb7d6459ce05c75c21cd4a07e18cfbeb9136628
cge1023 <ge931023@gmail.com>
2022-03-08 21:28:33 -0800

    app.py 업데이트

--- app.py
+++ app.py
@@ -33,19 +33,6 @@ def login():
     msg = request.args.get("msg")
     return render_template('login.html', msg=msg)
 
-# @app.route('/user/<username>')
-# def user(username):
-#     # 각 사용자의 프로필과 글을 모아볼 수 있는 공간
-#     token_receive = request.cookies.get('mytoken')
-#     try:
-#         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-#         status = (username == payload["id"])  # 내 프로필이면 True, 다른 사람 프로필 페이지면 False
-#
-#         user_info = db.users.find_one({"username": username}, {"_id": False})
-#         return render_template('user.html', user_info=user_info, status=status)
-#     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-#         return redirect(url_for("home"))
-
 
 @app.route('/sign_in', methods=['POST'])
 def sign_in():
@@ -67,32 +54,6 @@ def sign_in():
     else:
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
-
-# @app.route('/sign_up/save', methods=['POST'])
-# def sign_up():
-#     # 회원가입
-#     username_receive = request.form['username_give']
-#     password_receive = request.form['password_give']
-#     password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
-#     # DB에 저장
-#     return jsonify({'result': 'success'})
-#
-#
-# @app.route('/sign_up/check_dup', methods=['POST'])
-# def check_dup():
-#     # ID 중복확인
-#     return jsonify({'result': 'success'})
-
-
-# @app.route('/update_profile', methods=['POST'])
-# def save_img():
-#     token_receive = request.cookies.get('mytoken')
-#     try:
-#         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-#         # 프로필 업데이트
-#         return jsonify({"result": "success", 'msg': '프로필을 업데이트했습니다.'})
-#     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-#         return redirect(url_for("home"))
 
 @app.route('/posts')
 def comment():
