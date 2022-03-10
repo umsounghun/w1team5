93ba6b6adc4fe5f309fde61d494cd35c89060cc2
Sounghun Um <korea970112@naver.com>
2022-03-10 09:09:17 +0900

    회원가입 저장 틀 수정을 추가하였습니다.

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
 SECRET_KEY = 'SPARTA'
 
 
-client = MongoClient('mongodb+srv://gotgam:sparta@cluster0.k5twj.mongodb.net/Cluster0?retryWrites=true&w=majority')
+client = MongoClient('mongodb+srv://test:sparta@cluster0.e5mxe.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
 global doc
 
